d5c8036f25b774800588021fa67d07316f14c475
GBN6 <czesiox8@gmail.com>
2022-04-22 18:59:46 +0200

    Do simple rock paper scissors console game

--- rps_scripts.js
+++ rps_scripts.js
@@ -1,4 +1,6 @@
 alert('Hi! This is a simple Rock Paper Scissors game.')
+let playerScore = 0;
+let compScore = 0;
 
 function computerPlay() {
     let computerChoice = Math.floor(Math.random() * 3) + 1;
@@ -12,11 +14,82 @@ function computerPlay() {
 }
 
 function playerSelection() {
-    let playerChoice = prompt('Please choose between: "Rock", "Paper" or "Scissors"', '');
-    
-    return playerChoice;
+    let keepGoing = true
+    while (keepGoing) {
+    let playerChoice = prompt('Please choose between: "Rock", "Paper" or "Scissors"', '').trim().toLowerCase();
+        if (playerChoice === 'rock' || playerChoice === 'paper' || playerChoice === 'scissors'){
+            return playerChoice;
+        }
+        else {
+            alert('Please type only "Rock", "Paper" or "Scissors"');
+        }
+    }
 }
 
+function playRound(computerChoice, playerChoice) {
 
-console.log(playerSelection());
-console.log(computerPlay());
+    if (computerChoice === 'rock' && playerChoice === 'rock') 
+    {
+        return `Draw! Computer chose ${computerChoice} and you chose ${playerChoice}`;
+    }
+    else if (computerChoice === 'paper' && playerChoice === 'paper') 
+    {
+        return `Draw! Computer chose ${computerChoice} and you chose ${playerChoice}`;
+    }
+    else if (computerChoice === 'scissors' && playerChoice === 'scissors') 
+    {
+        return `Draw! Computer chose ${computerChoice} and you chose ${playerChoice}`;
+    }
+    else if (computerChoice === 'rock' && playerChoice === 'paper') 
+    {
+        playerScore++;
+        return `You won! Computer chose ${computerChoice} and you beat him by choosing ${playerChoice}`;
+    }
+    else if (computerChoice === 'rock' && playerChoice === 'scissors') 
+    {
+        compScore++
+        return `You lost! Computer chose ${computerChoice} and you ${playerChoice}`;
+    }
+    else if (computerChoice === 'paper' && playerChoice === 'rock') 
+    {
+        compScore++;
+        return `You lost! Computer chose ${computerChoice} and you ${playerChoice}`;
+    }
+    else if (computerChoice === 'paper' && playerChoice === 'scissors') 
+    {
+        playerScore++;
+        return `You won! Computer chose ${computerChoice} and you beat him by choosing ${playerChoice}`;
+    }
+    else if (computerChoice === 'scissors' && playerChoice === 'rock') 
+    {
+        playerScore++;
+        return `You won! Computer chose ${computerChoice} and you beat him by choosing ${playerChoice}`;
+    }
+    else if (computerChoice === 'scissors' && playerChoice === 'paper') 
+    {
+        compScore++;
+        return `You lost! Computer chose ${computerChoice} and you ${playerChoice}`;
+    }
+}
+
+function game() {
+
+    for (i = 0; i < 5; i++) {
+        result = playRound(computerPlay(), playerSelection());
+        console.log(result);
+        console.log(`Current score is ${playerScore} for player and ${compScore} for computer`);
+    }
+    if (playerScore > compScore) {
+        console.log(`Congratulations! You won ${playerScore} to ${compScore} `);
+    }
+    else if (playerScore = compScore) {
+        console.log(`OMG! It's a draw ${playerScore} to ${compScore} `);
+    }
+    else {
+        console.log(`Im so sorry, but you lost ${compScore} to ${playerScore}`);
+    }
+    
+    
+}
+ game();
+
