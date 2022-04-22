0eb35ce76c951be766e11a221460549c841d0ac6
GBN6 <czesiox8@gmail.com>
2022-04-22 17:18:01 +0200

    Create basic html page and prompt
    I've done a HTML page just to test my function
    randomly choosing for computer and then
    basic prompt asking user for his choice.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # RockPaperScisors
+This will be a simple prompt rock paper scissors game
+
+##
+Testing my basic JavaScript skills
+User will be asked to input choice
+Opponent will be a computer.
--- rps_scripts.js
+++ rps_scripts.js
@@ -1,1 +1,22 @@
-console.log('hello world')
+alert('Hi! This is a simple Rock Paper Scissors game.')
+
+function computerPlay() {
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
+    if (computerChoice === 1) {
+        return 'rock';
+    }
+    else if (computerChoice === 2) {
+        return 'paper';
+    }
+    else return 'scissors';
+}
+
+function playerSelection() {
+    let playerChoice = prompt('Please choose between: "Rock", "Paper" or "Scissors"', '');
+    
+    return playerChoice;
+}
+
+
+console.log(playerSelection());
+console.log(computerPlay());
